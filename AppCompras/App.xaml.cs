cec37e1219c0ca3c70c17407a3398105d78e82de
Carlos Orjuela <ingkrlosorjuela@evertecinc.com>
2022-12-21 15:35:57 -0500

    lista para trabajar

---
+++ AppCompras/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace AppCompras;
+
+public partial class App : Application
+{
+    public new static App Current => (App)Application.Current;
+    public IServiceProvider ServiceProvider { get; }
+
+    public App()
+    {
+        InitializeComponent();
+        MainPage = new AppShell();
+    }
+}
+
